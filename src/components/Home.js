6556d44607234f80bbbc285bac948826c1a513b4
vijay-jawalkar <vijay.jawalkar.786@gmail.com>
2024-01-05 11:34:45 +0530

    Added Feat : Toggle between C & F

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ function Home() {
     data: {},
     error: null,
   });
+  const [isChecked, setIsChecked] = useState(false);
 
   useEffect(() => {
     const handleSearch = async () => {
@@ -75,6 +76,22 @@ function Home() {
         />
       </form>
 
+      <div className="pl-4">
+        <label className="relative inline-flex items-center mb-5   cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            className="sr-only peer"
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+          />
+          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ms-3 text-sm text-gray-900 dark:text-gray-300 font-semibold">
+            {isChecked ? "Switch To Celsius " : "Switch To Fahrenheit"}
+          </span>
+        </label>
+      </div>
+
       {weather.loading && weather.data.location && (
         <div className="flex flex-col justify-center items-center py-16">
           <div className="flex flex-col justify-center items-center">
@@ -95,9 +112,12 @@ function Home() {
               />
             </div>
             <div>
-              {weather.data.current.temp_c}
+              {isChecked
+                ? Math.round(weather.data.current.temp_f)
+                : Math.round(weather.data.current.temp_c)}
+
               <sup>o</sup>
-              <span className="px-2">C</span>
+              <span className="px-2">{isChecked ? "F" : "C"}</span>
             </div>
           </div>
 
